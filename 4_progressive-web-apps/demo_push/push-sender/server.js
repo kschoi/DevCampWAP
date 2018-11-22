07898cb32e0f6c0daaa34d257c38c2dfd3bb1625
kyungsun choi <kschoi0308@gmail.com>
2018-11-22 16:54:21 +0900

    변경했어.

--- 4_progressive-web-apps/demo_push/push-sender/server.js
+++ 4_progressive-web-apps/demo_push/push-sender/server.js
@@ -3,7 +3,7 @@ var request = require('request');
 var serverKey =
   "key=" + "AAAAc5bgznU:APA91bESpXRIt2bb2et6GZIS5yMSdbIZ6uPHtFd02FIfa3mzZY5DYrxRT5tcZP_wMvn0_1Pev-M4xJZOqX4hcx6nty6qESirHim7GiyG3l4GBaW_vgOi_NEUk5PZSqL4Oaby_rcy_F-7";
 var deviceKey =
-  "cGrmgOcHugQ:APA91bGNNbFIz8kWHUktaDja43TjaocZwhGUaLVy-N7P0VHxBkoEdkd-M0HvHXCOcGwdqfqgwOc8PSEg_AcVh1cCMQNYATPP3of0BhXyhMHdm1tjZI6zcRSW6Qf_gTKIqxYqUC78qpqu";
+  "dxng6MHmWVM:APA91bE4S5snMsT4lBe1nDTyISPQS-8RuoBfN7Cm21ahZMt3O5DhJFdb6IlEqvZBxTJlb11h3H_qYtqxQqNZ5rFU9GEhg6sY2WQv7tsfWSJYWv-Clfw79we7dNqTJvL-olhwfu2e39po";
 sendMessageToUser(serverKey, deviceKey);
 
 function sendMessageToUser(serverKey, deviceKey, message) {
